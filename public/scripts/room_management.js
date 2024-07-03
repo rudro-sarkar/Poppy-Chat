f93dbf34a4a8ad1e897180f5d3a8590cfe7b2304
rudro-sarkar <rudrosarkar2327fb@gmail.com>
2024-07-03 11:42:13 +0600

    Update room_management.js

--- public/scripts/room_management.js
+++ public/scripts/room_management.js
@@ -11,11 +11,6 @@ const iceConfiguration = {}
 iceConfiguration.iceServers = [
    {
      urls: 'stun:stun1.l.google.com:19302'
-   },
-   {
-     urls: 'turn:openrelay.metered.ca:80',
-     username: 'openrelayproject',
-     credentials: 'openrelayproject'
    }
 ];
 
